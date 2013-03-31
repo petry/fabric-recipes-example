20d5573c70123acca9084beeebe3e8966aaf49ef
Marcos Daniel Petry <marcospetry@gmail.com>
2013-03-30 21:21:07 -0300

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,9 @@
 import os
 from fabric.decorators import task
 from fabric.api import env
-from fabricdeploy.functions import nginx_setup_server, python_enviroment, gunicorn_setup, \
-    create_folder_for_project, upload_project, install_pip_dependancies, gunicorn_deploy, \
-    nginx_setup_site, collect_static
-
-from fabricdeploy.fabutils import server_upgrade, puts, install_packages
 from os.path import join
+
+from recipes import DjangoDeploy
 
 env.project_name = "my_project"
 env.project_domain = "my_project.com"
@@ -26,32 +23,14 @@ env.remote_release_path = join(env.remote_app_path, 'releases')
 env.remote_current_path = join(env.remote_app_path, 'current')
 
 
-def instaling_project_dependencies():
-    install_packages(
-        [
-            'git-core',
-            'python-virtualenv'
-        ]
-    )
+my_deploy = DjangoDeploy()
 
 
 @task
 def setup():
-    server_upgrade()
-    instaling_project_dependencies()
-    nginx_setup_server()
-    gunicorn_setup()
-    puts('server setup done, running deploy')
-    deploy()
+    my_deploy.setup()
 
 
 @task
 def deploy():
-    puts("Deploying Project...")
-    create_folder_for_project()
-    python_enviroment()
-    upload_project()
-    install_pip_dependancies()
-    collect_static()
-    gunicorn_deploy()
-    nginx_setup_site()
+    my_deploy.deploy()
